70c18aaca6dd02f0a9f8f35696fb893358852fc1
Jack Li <jack.lee2980@gmail.com>
2022-01-01 20:42:30 -0500

    add usage to dev script

--- dev.sh
+++ dev.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="${0:a:h}"
+SCRIPT_NAME=$0
 
 SESSION_NAME="wechat_markdown_dev"
 
@@ -17,10 +18,43 @@ TMUX_COMMANDS=(
 	detach \;
 )
 
+function usage() {
+	>&2 cat <<-EOF
+	Usage: $SCRIPT_NAME [-s]
 
-if  (tmux list-sessions -F '#S' | grep "$SESSION_NAME") &>/dev/null; then
+	Starts/stops a named tmux session running the 'dev' and 'build-watch' yarn 
+	commands.  The session name is '$SESSION_NAME'.  If no arguments are 
+	specified the session is restarted.
+
+	Options:
+		-s          Stop the session only.
+		-h          Show help.
+	EOF
+}
+
+action=start
+
+while getopts ":s?h" c; do
+	case $c in
+		s)
+			action=stop
+			;;
+		?|h)
+			usage
+			exit 2
+			;;
+		*)
+			echo $c
+			;;
+	esac
+done
+
+if (tmux list-sessions -F '#S' | grep "$SESSION_NAME") &>/dev/null; then
 	echo "Killing $SESSION_NAME"
 	tmux kill-session -t "$SESSION_NAME"
 fi
 
-exec tmux $TMUX_COMMANDS
+if [[ $action == "start" ]]; then
+	echo "Starting $SESSION_NAME"
+	exec tmux $TMUX_COMMANDS
+fi
