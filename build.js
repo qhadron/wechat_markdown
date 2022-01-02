a471f4337ddef5de50212eb18bd374e1cf535ffa
Jack Li <jack.lee2980@gmail.com>
2022-01-02 05:00:51 -0500

    update dev system
    - use esbuild live server instead of watched builds / reload
    - use esbuild plugin system for renaming and logging etc
        - https://esbuild.github.io/plugins/
    - add plugin for moving assets (in assets folder and url starts with
      `/assets/`) to a separate folder in the output.

--- build.js
+++ build.js
@@ -1,103 +1,193 @@
 //@ts-check
-import {build as _build} from 'esbuild';
-import {join} from 'path';
-import {rename, rm} from 'fs/promises';
-import {dirname} from 'path';
+import {build, serve} from 'esbuild';
+import {join, basename} from 'path';
+import {cp, rename, opendir, rm} from 'fs/promises';
+import {dirname, resolve as realpath, relative} from 'path';
 import {fileURLToPath} from 'url';
 import htmlPlugin from '@chialab/esbuild-plugin-html';
 import clc from 'cli-color';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const __dist = join(__dirname, 'dist');
-const isDev = process.env.ENV == 'dev';
-const isWatch = process.argv.filter(arg => arg == '--watch').length > 0;
+const isDev = process.env.NODE_ENV == 'development';
 
-const workerEntryPoints = [
-	'vs/language/json/json.worker.js',
-	'vs/language/css/css.worker.js',
-	'vs/language/html/html.worker.js',
-	'vs/language/typescript/ts.worker.js',
-	'vs/editor/editor.worker.js'
-];
+const isServe = process.argv.some(arg => arg == '--serve');
 
 /**
- * by default, the index.html from the build is appeneded with a hash.
- * this is inconvenient and unnecessary.
- *
- * @param {import('esbuild').BuildResult} res
+ * @type {import('esbuild').Plugin}
  */
-async function renameIndexHtml(res) {
-	// remove hash for index.html file
-	const oldName = Object.entries(res.metafile.outputs)
-		.filter(([_, {inputs}]) => 'src/index.html' in inputs)
-		.map(([key, _]) => key)
-	[0];
-	if (!oldName) return res;
-	const newName = join(__dist, 'index.html');
-	await rename(oldName, newName);
-	return res;
+const renameEntryHtml = {
+	name: 'rename entry html',
+	setup(build) {
+		build.initialOptions.metafile = true;
+		const entryPoints = build.initialOptions.entryPoints;
+		const entries = entryPoints instanceof Array ? entryPoints :
+			Object.values(entryPoints);
+
+		if (entries) { 
+			build.onEnd(async result => { 
+				if (!result.metafile)
+					return;
+				await Promise.all(Object.entries(result.metafile.outputs)
+					.map(([out, {inputs}]) => {
+						const entry = entries.find(entry_1 => entry_1 in inputs);
+						if (!entry)
+							return Promise.resolve();
+						const dest = join(__dist, 'index.html');
+						const oldStats = result.metafile.outputs[out];
+						delete result.metafile.outputs[out];
+						result.metafile.outputs[relative(__dirname, __dist)] = oldStats;
+						return rename(out, dest);
+					}));
+			});
+		}
+	}
+};
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const moveAssetsPlugin = {
+	name: 'move assets',
+	setup(build) {
+		const publicPath = build.initialOptions.publicPath ?? '';
+		const destDir = __dist;
+
+		build.onResolve(
+			{filter: new RegExp(`^/assets/`), namespace: 'file'},
+			async args => {
+				const basePath = args.path.slice(1);
+				const real = realpath(__dirname, basePath);
+				const dest = realpath(destDir, basePath);
+				await cp(real, dest, {
+					dereference: true,
+					preserveTimestamps: true,
+					force: true,
+				});
+				return {
+					path: `${publicPath}/${basePath}`,
+					namespace: 'assets',
+					watchFiles: [real],
+					external: true,
+				};
+			}
+		);
+	}
 }
 
 /**
- * @param {import('esbuild').BuildResult} result
+ * @type {() => import('esbuild').Plugin}
  */
-function logBuildResult(result) {
-	console.log(clc.yellow.bold(`Build at ${new Date()}`));
-	Object.entries(result.metafile.outputs).map(([output, {inputs}]) => {
-		Object.keys(inputs)
-			.filter(x => !x.startsWith('node_modules'))
-			.map(input => console.log(clc.greenBright(`${input} => ${output}`)));
-	});
-	if (result.warnings.length > 0) {
-		console.warn(result.warnings);
-	}
-	if (result.errors.length > 0) {
-		console.error(result.errors);
-	}
-	return result;
+const loggerPlugin = () => {
+	return {
+		name: 'logger',
+		setup(build) {
+			let start;
+			let type;
+
+			build.onStart(() => {
+				type = basename(build.initialOptions.outdir);
+				if (type == 'dist')
+					type = 'all';
+				if (!start) {
+					start = new Date();
+					console.info(clc.yellow(`Build ${type} started at ${start}`));
+				}
+			});
+
+			build.onEnd(async result => {
+				const duration = Date.now() - start.getTime();
+				console.info(clc.yellow(`Build ${type} took: ${duration}ms`));
+				Object.entries(result.metafile.outputs).map(([output, {inputs}]) => {
+					Object.keys(inputs)
+						.filter(x => !x.startsWith('node_modules'))
+						.map(input => console.log('  ' + clc.greenBright(`${input} => ${output}`)));
+				});
+				if (result.warnings.length > 0) {
+					console.warn(result.warnings);
+				}
+				if (result.errors.length > 0) {
+					console.error(result.errors);
+				}
+			});
+		},
+	};
 }
 
-const buildPromises = [
-	build({
-		entryPoints: workerEntryPoints.map((entry) => `./node_modules/monaco-editor/esm/${entry}`),
-		bundle: true,
-		format: 'iife',
-		outbase: './node_modules/monaco-editor/esm/',
-		outdir: __dist,
-		metafile: true,
-	}),
-	build({
-		entryPoints: ['src/index.html'],
-		minify: !isDev,
-		bundle: true,
-		sourcemap: isDev,
-		platform: "browser",
-		watch: isWatch && {
-			onRebuild(err, res) {
-				logBuildResult(res);
-				return renameIndexHtml(res);
+/**
+ * @type {() => import('esbuild').Plugin}
+ */
+const cleanPlugin = () => {
+	const cleanDist = async () => {
+		let pending = [];
+		for await (const dir of await opendir(__dist)) {
+			const path = realpath(__dist, dir.name);
+			console.log(clc.red(`Removing dirty file ${clc.green(path)}`))
+			pending.push(rm(path, {
+				force: true,
+				recursive: true,
+			}));
+		}
+		await Promise.all(pending);
+	};
+	return {
+		name: 'clear',
+		async setup(build) {
+			if (isDev) return;
+
+			const entryPoints = build.initialOptions.entryPoints;
+			const entryPoint = entryPoints instanceof Array ?
+				entryPoints :
+				Object.keys(entryPoints);
+			if (entryPoint[0].endsWith('html')) {
+				await cleanDist();
 			}
 		},
-		plugins: [
-			htmlPlugin({
-
-			}),
-		],
-		outdir: __dist,
-		loader: {
-			'.ttf': 'file',
-			'.md': 'text',
-		},
-		metafile: true,
-	}).then(logBuildResult).then(renameIndexHtml),
-];
+	};
+};
 
 /**
- * @param {import ('esbuild').BuildOptions} opts
+ * @type {import('esbuild').BuildOptions}
  */
-async function build(opts) {
-	const result = await _build(opts);
-	return result;
-}
+const buildOptions = {
+	entryPoints: {
+		'index.html': 'src/index.html',
+	},
+	outdir: __dist,
 
-await Promise.all(buildPromises);
+	minify: !isDev,
+	bundle: true,
+	platform: "browser",
+	sourcemap: isDev,
+	target: [
+		"es2020",
+		"chrome96",
+		"firefox95",
+	],
+	drop: isDev ? [] : ['debugger', 'console'],
+	metafile: true,
+
+	loader: {
+		'.txt': 'text',
+		'.ttf': 'file',
+		'.png': 'file',
+	},
+
+	plugins: [
+		cleanPlugin(),
+		htmlPlugin({}),
+		moveAssetsPlugin,
+		renameEntryHtml,
+		loggerPlugin(),
+	],
+};
+
+if (isServe) {
+	serve({
+		servedir: __dist,
+		port: 8000,
+		host: 'localhost',
+	}, buildOptions)
+} else {
+	build(buildOptions);
+}
--- dev.sh
+++ dev.sh
@@ -10,8 +10,7 @@ SESSION_NAME="wechat_markdown_dev"
 TMUX_COMMANDS=(
 	new-session -c "$SCRIPT_DIR" -s "$SESSION_NAME";
 	set-option -w remain-on-exit on \;
-	split-window -h -c "$SCRIPT_DIR" zsh -c 'source ~/.zshrc; yarn run build-watch' \;
-	split-window -h -c "$SCRIPT_DIR" zsh -c 'source ~/.zshrc; yarn run dev' \;
+	split-window -h -c "$SCRIPT_DIR" zsh -c 'source ~/.zshrc; yarn run start-dev' \;
 	set-option -w remain-on-exit on \;
 	select-layout even-horizontal \;
 	kill-pane -t "{left}" \;	
